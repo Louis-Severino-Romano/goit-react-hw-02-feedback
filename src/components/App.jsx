b9e8d5d072cbb744ff75db8ffb002fc54d8d5372
Louis-Severino-Romano <louis.romano@ctu.edu.ph>
2024-06-13 16:48:11 +0800

    ini5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,59 @@
-
-import {Component} from 'react'; 
+import { Component } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 
 export class App extends Component {
-  state={
-    Good:0,
-    Bad:0,
-    Neutral:0,
-  }
+  state = {
+    Good: 0,
+    Bad: 0,
+    Neutral: 0,
+  };
 
-  countTotalFeedback = ()=>{
-    const {Good, Neutral, Bad}=this.state;
+  countTotalFeedback = () => {
+    const { Good, Neutral, Bad } = this.state;
     return Good + Neutral + Bad;
-  }
+  };
 
-  countPositiveFeedbackPercentage=()=>{
+  countPositiveFeedbackPercentage = () => {
     const Total = this.countTotalFeedback();
-    return Math.round((this.state.Good/Total)*100);
-  }
+    return Math.round((this.state.Good / Total) * 100);
+  };
 
-  handleClick=type=>{
-    this.setState(prevState=>({
+  handleClick = type => {
+    this.setState(prevState => ({
       ...prevState,
-      [type]:prevState[type]+1,
-    }))
-
-  }
+      [type]: prevState[type] + 1,
+    }));
+  };
 
   render() {
-    const {Good, Neutral, Bad}=this.state;
-    let Total=this.countTotalFeedback();
-   const Percentage=this.countPositiveFeedbackPercentage();
-    const options=['Good', 'Neutral', 'Bad'];
-    return <div>
+    const { Good, Neutral, Bad } = this.state;
+    const Total = this.countTotalFeedback();
+    const Percentage = this.countPositiveFeedbackPercentage();
+    const options = ['Good', 'Neutral', 'Bad'];
 
-      <Section title='Please Leave a Feedback'>
-      <FeedbackOptions options={options} onLeaveFeedback={this.handleClick}/>
-      </Section>
-      <Section title='Statistics'>
-     <Statistics Good={Good} Neutral={Neutral} Bad={Bad} Total={Total} Percentage={Percentage}/> 
-     </Section>
-    </div>;
+ 
+    
+    return (
+      <div>
+        <Section title="Please Leave a Feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.handleClick}
+          />
+          
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            Good={Good}
+            Neutral={Neutral}
+            Bad={Bad}
+            Total={Total}
+            Percentage={Percentage}
+          />
+        </Section>
+      </div>
+    );
   }
 }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,7 +1,9 @@
 
 
 export const Statistics = ({Good, Neutral, Bad, Total, Percentage}) => {
-  return (
+    
+    
+    return (
     <div><p>Good <span>{Good}</span></p>
     <p>Neautral <span>{Neutral}</span></p>
     <p>Bad <span>{Bad}</span></p>
