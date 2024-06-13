0f9a105f8fac0aecdb47a3d7271ed1f97bb1526d
Louis-Severino-Romano <louis.romano@ctu.edu.ph>
2024-06-13 14:36:23 +0800

    ini2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,43 @@
+import { type } from '@testing-library/user-event/dist/type';
 import {Component} from 'react'; 
 export class App extends Component {
   state={
-    good:3,
-    bad:0,
-    neutral:1,
+    Good:0,
+    Bad:0,
+    Neutral:0,
   }
 
   countTotalFeedback = ()=>{
-    const {good, neutral, bad}=this.state;
-    return good + neutral + bad;
+    const {Good, Neutral, Bad}=this.state;
+    return Good + Neutral + Bad;
   }
 
   countPositiveFeedbackPercentage=()=>{
     const Total = this.countTotalFeedback();
-    return this.state.good/Total;
+    return this.state.Good/Total;
+  }
+
+  handleClick=type=>{
+    this.setState(prevState=>({
+      ...prevState,
+      [type]:prevState[type]+1,
+    }))
+
   }
 
   render() {
-    const {good, neutral, bad}=this.state;
+    const {Good, Neutral, Bad}=this.state;
     let Total=this.countTotalFeedback();
     let Percentage = this.countPositiveFeedbackPercentage()*100;
-
+    const options=['Good', 'Neutral', 'Bad'];
     return <div>
-      <button>Good</button>
-      <button>Neutral</button>
-      <button>Bad</button>
-      <p>Good <span>{good}</span></p>
-      <p>Neautral <span>{neutral}</span></p>
-      <p>Bad <span>{bad}</span></p>
+      {options.map(option=>(
+        <button key={option} onClick={()=>this.handleClick(option)}> {option}</button>
+      ))};
+
+      <p>Good <span>{Good}</span></p>
+      <p>Neautral <span>{Neutral}</span></p>
+      <p>Bad <span>{Bad}</span></p>
       <p>Total <span>{Total}</span></p>
       <p>Percentage <span>{Percentage}%</span></p>
     </div>;
