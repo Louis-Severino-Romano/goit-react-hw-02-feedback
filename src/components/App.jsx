ea11a3af9e6b600f69c67c43f6f9ce71f536266c
Louis-Severino-Romano <louis.romano@ctu.edu.ph>
2024-06-13 11:52:59 +0800

    ini

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import {Component} from 'react'; 
+export class App extends Component {
+  state={
+    good:3,
+    bad:0,
+    neutral:1,
+  }
+
+  countTotalFeedback = ()=>{
+    const {good, neutral, bad}=this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage=()=>{
+    const Total = this.countTotalFeedback();
+    return this.state.good/Total;
+  }
+
+  render() {
+    const {good, neutral, bad}=this.state;
+    let Total=this.countTotalFeedback();
+    let Percentage = this.countPositiveFeedbackPercentage()*100;
+
+    return <div>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+      <p>Good <span>{good}</span></p>
+      <p>Neautral <span>{neutral}</span></p>
+      <p>Bad <span>{bad}</span></p>
+      <p>Total <span>{Total}</span></p>
+      <p>Percentage <span>{Percentage}%</span></p>
+    </div>;
+  }
+}
